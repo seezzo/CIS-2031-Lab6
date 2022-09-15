5911e211bbef7aa9e3895920539b9756486ea4ee
Sarah Ezzo <see09150@csc.vsc.edu>
2022-09-15 10:02:02 -0400

    added minFunction and whileLoop

--- Lab6.cpp
+++ Lab6.cpp
@@ -16,28 +16,33 @@ int max(int a, int b){
 
 // LAB QUESTION 3 - Write a function with an if statement
 
-//    (YOUR CODE HERE)
+int min(int a, int b){
+    if ( a < b ){
+        return a;
+    } else {
+        return b;
+    }
+}
 
 
 void main(){
     cout << "Which is larger, 42 or 96? " << max(42, 96) << endl;
 
     // LAB QUESTION 3 - Uncomment the next line
-    //cout << "Which is smaller, 12 or 100? " << min(12, 100) << endl;
+    cout << "Which is smaller, 12 or 100? " << min(12, 100) << endl;
 
     //LAB QUESTION 5 - Comment out the next line
-    return;
+    //return;
 
     //LAB QUESTIONS 6 & 7 is about this code:
 
     //Starting at zero, count to 10
     cout << "Couning to ten!" << endl;
     int number = 0;
-    tryTheNextNumber:
-    if ( number <= 10 ){
+    
+    while ( number <= 10 ){
         cout << "In the loop, number = " << number << endl;
         number = number + 1;
-        goto tryTheNextNumber;
     }
 
     cout << "I counted to ten, and the number variable = " << number << endl;
